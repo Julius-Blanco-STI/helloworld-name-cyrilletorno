a355a8c6efa049387c1184823c4c2169c78c2a46
cyrilletorno <58576014+cyrilletorno@users.noreply.github.com>
2019-12-06 08:45:54 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+
+package main;
+
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    public static void main(String[] args) {
+        String name = "Cyrille";
+        System.out.println("Hello world" + name);
+        
+        
+    }
+    
 }
