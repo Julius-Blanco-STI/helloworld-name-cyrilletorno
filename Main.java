4d550cc2c7e5583bf5b78a89a4e57798ce458674
cyrilletorno <58576014+cyrilletorno@users.noreply.github.com>
2019-12-07 17:08:56 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 package main;
 public class Main {
      public static void main(String[] args) {
-        String name = "Cyrille";
-        System.out.println("Hello world" + name);
+     String name = "Cyrille";
+     System.out.println("Hello world" + name);
    }
 }
